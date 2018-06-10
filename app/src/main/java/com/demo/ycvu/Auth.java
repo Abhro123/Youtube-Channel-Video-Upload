eda45aeab9e608851f664e5d92608de2f84fda34
Abhro <abhrobhattacharjee@gmail.com>
2018-06-10 21:16:15 +0530

    First commit

--- app/src/main/java/com/demo/ycvu/Auth.java
+++ app/src/main/java/com/demo/ycvu/Auth.java
@@ -17,6 +17,8 @@ package com.demo.ycvu;
 import com.google.android.gms.common.Scopes;
 import com.google.api.services.youtube.YouTubeScopes;
 
+
+
 public class Auth {
     // Register an API key here: https://console.developers.google.com
     public static final String KEY = "AIzaSyDPKYmCZFs0JGOtd8vRk96nkwIDFf0Tcqs";
